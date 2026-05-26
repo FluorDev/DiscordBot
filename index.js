c1bb37a190767323cd9b372827a3818403829355
Người Tình Mùa Hạ <FluorDev@proton.me>
2026-05-26 21:20:19 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const {
+  Client,
+  GatewayIntentBits
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} online!`);
+});
+
+client.on("messageCreate", (msg) => {
+
+  if(msg.content === "!ping") {
+    msg.reply("Pong!");
+  }
+
+});
+
+client.login(process.env.TOKEN);
